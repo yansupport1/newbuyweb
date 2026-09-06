af9a09f22731455f91b76299fde04262480fcb20
yansupport1 <yansupport1@gmail.com>
2026-09-06 14:34:34 +0700

    Last Sync: 2026-09-06 14:34 (Mobile)

--- js/security.js
+++ js/security.js
@@ -1,11 +1,7 @@
-/* YX-SEC protected */
-(function(){
-var _k=90,_s=
-"cjwvNDkuMzU0enJzeiFQenp9Lyk/eikuKDM5Ln1hUHp6LigjeiFQenp6ejk1NCk1Nj90NjU9cn1/OQM7NCB6AjMu"
-+"PygpegkuNSg/fXZ6fTk1NjUoYHk8PGs7aT5hPDU0LncpMyA/YGtsKiJhPDU0LnctPzM9Mi5gODU2Pn1zYVB6eid6"
-+"OTsuOTJ6cj9zeiEnUCdzcnNhUA=="
-;
-var _b=atob(_s),_o="";
-for(var i=0;i<_b.length;i++)_o+=String.fromCharCode(_b.charCodeAt(i)^_k);
-(0,eval)(_o);
+// Soft security - tidak merusak UI
+(function () {
+  "use strict";
+  try {
+    console.log("%cYanz Xiters Store", "color:#ff1a3d;font-size:16px;font-weight:bold");
+  } catch (e) {}
 })();
